a0c7f075a3c9338af89d30775ed7203b92b41f1c
Asiya Khassenova <bb84960@gmail.com>
2024-01-26 23:14:10 +0600

    hw lesson 3 django

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def product(request):
+    return render(request, 'product.html')
